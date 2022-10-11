a8e19726f8a73f10ba250495e49d14aaa346b94e
topgeradores <topgeradores.eng@gmail.com>
2022-10-11 11:13:52 -0300

    Script IaC v1

---
+++ README.md
@@ -0,0 +1,8 @@
+### Estrutura
+
+| Diretório | Dono (rwx) | Grupo (rwx) | Usuários (outros: - - -) |
+| ------ | ------ | ------ | ------ |
+| /publico | root |  |  |
+| /adm | root | GRP_ADM | carlos, maria, joao |
+| /ven | root | GRP_VEN | debora, sebastiana, roberto |
+| /sec | root | GRP_SEC | josefina, amanda, rogerio |
---
+++ iac.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+echo "Criando diretórios..."
+mkdir /publico
+mkdir /adm
+mkdir /ven
+mkdir /sec
+
+echo "Criando grupos..."
+groupadd GRP_ADM
+groupadd GRP_VEN
+groupadd GRP_SEC
+
+echo "Criando usuários..."
+
+useradd carlos -m -c "Carlos" -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+passwd carlos -e
+
+useradd maria -m -c "Maria" -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+passwd maria -e
+
+useradd joao -m -c "João" -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+passwd joao -e
+
+useradd debora -m -c "Debora" -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
+passwd debora -e
+
+useradd sebastiana -m -c "Sebastiana" -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
+passwd sebastiana -e
+
+useradd roberto -m -c "Roberto" -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
+passwd roberto -e
+
+useradd josefina -m -c "Josefina" -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
+passwd josefina -e
+
+useradd amanda -m -c "Amanda" -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
+passwd amanda -e
+
+useradd rogerio -m -c "Rogério" -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
+passwd rogerio -e
+
+echo "Especificando permissões dos diretórios..."
+
+chown root:GRP_ADM /adm
+chown root:GRP_VEN /ven
+chown root:GRP_SEC /sec
+
+chmod 770 /adm
+chmod 770 /ven
+chmod 770 /sec
+chmod 777 /publico
+
+echo "Fim!"
+
+
